96a71dfd8b62d7e7fb74fffebfc7deb3cc5bef72
Mike Schouten <mikeschouten@sp-byods-145-109-36-45.wireless.uva.nl>
2025-02-19 16:55:12 +0100

    test

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -1,28 +1,43 @@
+```
 import os
-import pymongo
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
-client = pymongo.MongoClient(os.environ['MONGO_URI'])
-db = client["university"]
-collection = db["students1"]
+# Could move this to .env file
+mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+client = MongoClient(mongo_uri)
+db = client.student_db
+students_collection = db.students
 
 def add(student=None):
-    if collection.find_one({"first_name": student.first_name, "last_name": student.last_name}) is not None:
-        return 'already exists', 409
+    # Always allow insert
+    # student_record = students_collection.find_one({
+    #     "first_name": student.first_name,
+    #     "last_name": student.last_name
+    # })
+    #
+    # if student_record:
+    #     return 'Student already exists', 409
 
-
-    idx = collection.insert_one(student.to_dict()).inserted_id
-    return str(idx), 200
+    res = students_collection.insert_one(student.to_dict())
+    student.student_id = str(res.inserted_id)
+    return student.student_id
 
 def get_by_id(student_id=None, subject=None):
-    student = collection.find_one({"_id": student_id})
+    student = students_collection.find_one({"_id": ObjectId(student_id)})
+
     if not student:
         return 'not found', 404
-    print(student)
+
+    student['student_id'] = str(student['_id'])
+    del student['_id']
     return student
 
 def delete(student_id=None):
-    student = collection.find_one({"_id": student_id})
-    if not student:
+    res = students_collection.delete_one({"_id": ObjectId(student_id)})
+
+    if res.deleted_count == 0:
         return 'not found', 404
-    collection.delete_one(student)
+
     return student_id
+```
